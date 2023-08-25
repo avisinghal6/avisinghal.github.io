b262b35c93d0ed66b213b403341f565b523dbe8f
avisinghal6 <avisinghal6@gmail.com>
2023-08-25 16:18:36 -0500

    updated css formatting

--- portfolio/src/components/homepage.js
+++ portfolio/src/components/homepage.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
 
 import {
-	faMailBulk,
-	faCode,
-	faHashtag,
-	faHammer,
-	faClipboard,
+  faMailBulk,
+  faCode,
+  faHashtag,
+  faHammer,
+  faClipboard,
 } from "@fortawesome/free-solid-svg-icons";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -22,79 +22,85 @@ import SEO from "../data/seo";
 import "../styles/pages/homepage/homepage.css";
 
 const Homepage = () => {
-    const [stayLogo, setStayLogo] = useState(false);
-	const [logoSize, setLogoSize] = useState(80);
-	const [oldLogoSize, setOldLogoSize] = useState(80);
+  const [stayLogo, setStayLogo] = useState(false);
+  const [logoSize, setLogoSize] = useState(100);
+  const [oldLogoSize, setOldLogoSize] = useState(100);
 
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-	useEffect(() => {
-		const handleScroll = () => {
-			let scroll = Math.round(window.pageYOffset, 2);
+  useEffect(() => {
+    const handleScroll = () => {
+      let scroll = Math.round(window.pageYOffset, 2);
 
-			let newLogoSize = 80 - (scroll * 4) / 10;
+      let newLogoSize = 80 - (scroll * 4) / 10;
 
-			if (newLogoSize < oldLogoSize) {
-				if (newLogoSize > 40) {
-					setLogoSize(newLogoSize);
-					setOldLogoSize(newLogoSize);
-					setStayLogo(false);
-				} else {
-					setStayLogo(true);
-				}
-			} else {
-				setLogoSize(newLogoSize);
-				setStayLogo(false);
-			}
-		};
+      if (newLogoSize < oldLogoSize) {
+        if (newLogoSize > 40) {
+          setLogoSize(newLogoSize);
+          setOldLogoSize(newLogoSize);
+          setStayLogo(false);
+        } else {
+          setStayLogo(true);
+        }
+      } else {
+        setLogoSize(newLogoSize);
+        setStayLogo(false);
+      }
+    };
 
-		window.addEventListener("scroll", handleScroll);
-		return () => window.removeEventListener("scroll", handleScroll);
-	}, [logoSize, oldLogoSize]);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [logoSize, oldLogoSize]);
 
-	const currentSEO = SEO.find((item) => item.page === "home");
+  const currentSEO = SEO.find((item) => item.page === "home");
 
-	const logoStyle = {
-		display: "flex",
-		position: stayLogo ? "fixed" : "relative",
-		top: stayLogo ? "3vh" : "auto",
-		zIndex: 999,
-		border: stayLogo ? "1px solid white" : "none",
-		borderRadius: stayLogo ? "50%" : "none",
-		boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
-	};
+  const logoStyle = {
+    display: "flex",
+    position: stayLogo ? "fixed" : "relative",
+    top: stayLogo ? "3vh" : "auto",
+    zIndex: 999,
+    border: stayLogo ? "1px solid white" : "none",
+    borderRadius: stayLogo ? "50%" : "none",
+    boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
+  };
 
-    return (
-        <div>
-            <Helmet>
-				<title>{INFO.main.title}</title>
-				<meta name="description" content={currentSEO.description} />
-				<meta
-					name="keywords"
-					content={currentSEO.keywords.join(", ")}
-				/>
-			</Helmet>
+  return (
+    <div>
+      <Helmet>
+        <title>{INFO.main.title}</title>
+        <meta name="description" content={currentSEO.description} />
+        <meta name="keywords" content={currentSEO.keywords.join(", ")} />
+      </Helmet>
 
-			<div className="page-content">
-				<NavBar active="home" />
-				<div className="content-wrapper">
-					<div className="homepage-logo-container">
-						<div style={logoStyle}>
-							<Logo width={logoSize} link={false} />
-						</div>
-					</div>
+      <div className="page-content">
+        <NavBar active="home" />
+        <div className="content-wrapper">
+          <div className="homepage-logo-container">
+            <div style={logoStyle}>
+              <Logo width={logoSize} link={false} />
+            </div>
+          </div>
 
-				<div className="page-footer">
-					<Footer />
-				</div>
-				</div>
-				
-			</div>
-		
+          <div className="homepage-name">Avi Singhal</div>
+
+          <div className="homepage-image-container">
+            <div className="homepage-image-wrapper">
+              <img
+                src={process.env.PUBLIC_URL + "/images/avi.png"}
+                alt="Avi Singhal"
+              />
+            </div>
+          </div>
         </div>
-    );
-}
+
+        <div className="page-footer">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Homepage;
