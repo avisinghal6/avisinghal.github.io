5dd19b288dbe493fea863581a05be9cfe458e79e
avisinghal6 <avisinghal6@gmail.com>
2023-08-24 22:51:21 -0500

    added pics, data

---
+++ portfolio/src/data/seo.js
@@ -0,0 +1,10 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am an aspiring software/ML engineer with strong foundations in state-of-art technologies like machine learning, deep learning and natural language processing. I have obtained solid software development knowledge by working on MERN stack and developing and deploying various projects.",
+		keywords: ["Avi", "Avi S", "Avi Singhal", "AS"],
+	},
+];
+
+export default SEO;
---
+++ portfolio/src/data/user.js
@@ -0,0 +1,10 @@
+const INFO = {
+	main: {
+		title: "Avi Singhal",
+		name: "Avi Singhal",
+		email: "as278@rice.edu",
+		logo: process.env.PUBLIC_URL + "/images/as.png",
+	},
+};
+
+export default INFO;
